a5efe60b9322c59debae8e68be9c3c2f12baf29c
Christian Alejandro Mosri <39068565+IngMosri@users.noreply.github.com>
2024-07-03 17:23:43 -0600

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 print("Enter a TO-DO list: ")
-Userinput1 = input( "Enter what do you want to add in your TO- DO list:\n")
-Userinput2 = input( "Enter what do you want to add in your TO- DO list:\n")
-Userinput3 = input( "Enter what do you want to add in your TO- DO list:\n")
-inputlist = [Userinput1,Userinput2,Userinput3]
 
-for valores, user_input in enumerate(inputlist):
-     print(f"{valores}: {user_input}")
-print ("Your task was save correctly :D")
+Userinput1 = "Enter what do you want to add in your TO-DO list:\n"
 
-   
+UserTodoList = []
 
+while True:
+    item = input(Userinput1)
+    UserTodoList.append(item)
+    print(UserTodoList)
